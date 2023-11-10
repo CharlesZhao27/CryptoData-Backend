3bb751cb2a14a623108338d1b0bc60dc252bb0a8
Weibo Zhao <106577352+WeiboZhao27@users.noreply.github.com>
2023-11-10 15:34:56 +1100

    fix: Flyway issue

--- src/main/resources/db/migration/V1__data_init.sql
+++ src/main/resources/db/migration/V1__data_init.sql
@@ -1,12 +1,12 @@
-create table "crypto_market_data"(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    symbol VARCHAR(255) NOT NULL,
-    date TIMESTAMP NOT NULL,
-    high NUMERIC(50,30),
-    low NUMERIC(50,30),
-    open NUMERIC(50,30),
-    close NUMERIC(50,30),
-    volume NUMERIC(80,30),
-    market_cap NUMERIC(80,30)
+CREATE TABLE "crypto_market_data"(
+    id         SERIAL          PRIMARY KEY  UNIQUE ,
+    name       VARCHAR(255)    NOT NULL            ,
+    symbol     VARCHAR(255)    NOT NULL            ,
+    date       TIMESTAMP       NOT NULL            ,
+    high       DECIMAL(50,30)                      ,
+    low        DECIMAL(50,30)                      ,
+    open       DECIMAL(50,30)                      ,
+    close      DECIMAL(50,30)                      ,
+    volume     DECIMAL(80,30)                      ,
+    market_cap DECIMAL(80,30)
 );
